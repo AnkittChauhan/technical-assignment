f473d4478a8d143fee759f21930851a7bfe2011f
Anass AZIZI ALAOUI <anass.azizi@nemo.ma>
2026-02-04 18:43:50 +0100

    fix: lint config

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,15 +1,25 @@
 import js from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 
 export default [
   js.configs.recommended,
   {
-    files: ["**/*.{ts,tsx}"],
+    files: ["**/*.ts", "**/*.tsx"],
     languageOptions: {
-      ecmaVersion: 2022,
-      sourceType: "module",
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: "module",
+        ecmaFeatures: { jsx: true },
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
     },
     rules: {
-      "no-console": "off"
-    }
-  }
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    },
+  },
 ];
